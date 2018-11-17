5c19720ffccc5bc5df7fb8ace037473126966314
juliusHin <julius.tanuwijaya@student.umn.ac.id>
2018-11-17 15:00:14 +0700

    angular, firebase
    Signed-off-by: juliusHin <julius.tanuwijaya@student.umn.ac.id>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
@@ -9,6 +10,11 @@ import { ListPage } from '../pages/list/list';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+// angular & firebase
+import {AngularFireModule} from 'angularfire2'
+import {AngularFireAuth} from 'angularfire2/auth';
+import { firebaseConfig } from './../providers/api/firebaseConfig';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -18,6 +24,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    // codingan ada di dalam folder /src/providers/api/firebaseConfig.ts
+    AngularFireModule.initializeApp(firebaseConfig.fire)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -28,7 +36,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AngularFireAuth
   ]
 })
 export class AppModule {}
---
+++ src/providers/api/firebaseConfig.ts
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+    fire: {
+        apiKey: "AIzaSyBSWCW5UoOjyJ8gtJs3i92H6MrFwr3zN7Y",
+        authDomain: "vendorpedia-6f595.firebaseapp.com",
+        databaseURL: "https://vendorpedia-6f595.firebaseio.com",
+        projectId: "vendorpedia-6f595",
+        storageBucket: "vendorpedia-6f595.appspot.com",
+        messagingSenderId: "37472993473"
+
+    }
+}
