2ac35e7f57500fa03036c8ecadcc45a473fc1195
kennyvince222 <44108998+kennyvince222@users.noreply.github.com>
2018-11-19 09:49:48 +0700

    Update signin.ts

--- src/pages/signin/signin.ts
+++ src/pages/signin/signin.ts
@@ -15,11 +15,56 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SigninPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  loginForm: FormGroup;
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public menuCtrl: MenuController,
+    public formBuilder: FormBuilder,
+    public clientsService: ClientsService,
+    public alertCtrl: AlertController,
+    private storage: Storage
+  ) {
+    this.loginForm = this.makeLoginForm();
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad SigninPage');
+    console.log('ionViewDidLoad LoginPage');
   }
 
+  ionViewDidEnter() {
+    this.menuCtrl.enable(false, 'menu');
+  }
+
+  doLogin(event: Event) {
+    event.preventDefault();
+    const username = this.loginForm.value.username;
+    const password = this.loginForm.value.password;
+    if(username == "kenny" && password == "123456"){
+      console.log(username, password);
+      const user = JSON.stringify(username);
+      this.storage.set('user', user);
+      this.navCtrl.setRoot('HomePage', {
+        user: username
+      }); 
+    }else{
+      const alert = this.alertCtrl.create({
+        title: 'Data tidak valid',
+        subTitle: 'Periksa data anda',
+        buttons: ['OK']
+      });
+      alert.present();
+    }
+    
+  }
+
+
+  private makeLoginForm() {
+    return this.formBuilder.group({
+      username: ['', [Validators.required, Validators.minLength(4)]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
 }
+
